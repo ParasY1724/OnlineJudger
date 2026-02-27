8a3a52f1ef59387c84fdca24a057f484395c7ebf
ParasY1724 <parasyerunkar174@gmail.com>
2026-02-27 18:30:40 +0530

    added dynamoDB table & SQS queues

--- codejudge-serverless/infrastructure/main.tf
+++ codejudge-serverless/infrastructure/main.tf
@@ -10,3 +10,23 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_dynamodb_table" "submissions" {
+  name         = "CodeJudgeSubmissions"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "submissionId"
+
+  attribute {
+    name = "submissionId"
+    type = "S"
+  }
+}
+
+resource "aws_sqs_queue" "submission_queue" {
+  name = "codejudge-submission-queue"
+  visibility_timeout_seconds = 30
+}
+
+resource "aws_sqs_queue" "result_queue" {
+  name = "codejudge-result-queue"
+}
